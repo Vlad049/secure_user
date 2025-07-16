9e38b95ec59129f2ba77639b1e0e51061c074b13
Vlad049 <vladbuksa7@gmail.com>
2025-07-16 18:56:38 +0300

    upd

--- pydantic_models.py
+++ pydantic_models.py
@@ -1,6 +1,9 @@
-from typing import Optional
+from typing import Annotated, Optional
+import string
+import re
+
 from pydantic_settings import BaseSettings
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 
 class Settings(BaseSettings):
@@ -8,29 +11,60 @@ class Settings(BaseSettings):
 
 
 class UserModel(BaseModel):
-    username: str
-    password: str
+    username: Annotated[str, Field(..., description="Логін користувача")]
+    password: Annotated[str, Field(..., min_length=8, description="Пароль користувача")]
+
+    @field_validator("password")
+    def password_validator(cls, value: str):
+        is_upper = False
+        is_lower = False
+        is_digit = False
+        is_punctuation = False
+
+        for char in value:
+            if not is_upper and char.isupper():
+                is_upper = True
+
+            if not is_lower and char.islower():
+                is_lower = True
+
+            if not is_digit and char.isdigit():
+                is_digit = True
+
+            if not is_punctuation and char in string.punctuation:
+                is_punctuation = True
+
+        if all([is_upper, is_lower, is_digit, is_punctuation]):
+            return value
+        
+        raise ValueError("Мінімум 8 символів, одну велику та маленьку літеру та обин спецсимвол")
 
 
 class UserResponse(UserModel):
-    id: str
-    token: Optional[str] = None
+    id: Annotated[int, Field(0, description="Айді користувача")]
+    token: Annotated[Optional[str], Field(None, description="Токен користувача")]
 
 
 class Token(BaseModel):
-    access_token: str
-    token_type: str = "bearer"
+    access_token: Annotated[str, Field(..., description="Токен користувача")]
+    token_type: Annotated[str, Field("bearer", description="Тип токену користувача")]
 
 
 class ContactModel(BaseModel):
-    name: str
-    phone_number: str
-    address: Optional[str] = None
-    email: Optional[str] = None
+    name: Annotated[str, Field(None, description="Ім'я користувача")]
+    phone_number: Annotated[Optional[str], Field(None, description="Номер телефону у форматі '+38(099)123-45-78'", examples=['+38(099)123-45-78'])]
+    address: Annotated[Optional[str], Field(None, description="Адреса користувача")]
+    email: Annotated[Optional[EmailStr], Field(None, description="Електронна пошта користувача")]
+
+    @field_validator("phone_number")
+    def phone_number_validator(cls, value: Optional[str]):
+        if value and not re.search(r"\+38\(0\d{2}\)\d{3}-\d{2}-\d{2}", value):
+            raise ValueError("Номер телефону повинен бути у форматі '+38(099)123-45-78'")
+        return value
 
 
 class ContactModelResponce(ContactModel):
-    id: str
-    user_id: str
+    id: Annotated[int, Field(0, description="Порядковий номер")]
+    user_id: Annotated[int, Field(0, description="Айді користувача")]
 
 settings = Settings()
